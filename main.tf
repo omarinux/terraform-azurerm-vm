3c74c3e7e8674abde2cf388a25ecfbf0b14c4dd1
Omar Germana <omarinux@gmail.com>
2023-08-15 17:19:10 +0200

    dd

--- main.tf
+++ main.tf
@@ -251,7 +251,7 @@ resource "azurerm_network_interface" "vm" {
     name                          = "ipconfig${count.index}"
     subnet_id                     = "${var.vnet_subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.vm.[count.index]}"
+    public_ip_address_id          = "${element(azurerm_public_ip.vm.*.id, count.index)}"
   }
 }
 
