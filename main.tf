d2cc691dde3e2467a93ef64157a7641d8e8d05de
Omar Germana <omarinux@gmail.com>
2023-08-16 10:39:07 +0200

    ip

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -120,7 +120,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "RDP"
-    priority                   = 1001
+    priority                   = 1003
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
