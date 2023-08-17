87a2a8bd1dcb75c7076f2109fc9dd494c38599f7
Omar Germana <omarinux@gmail.com>
2023-08-17 16:44:51 +0200

    WIN

--- ansible.tf
+++ ansible.tf
@@ -97,9 +97,9 @@ resource "null_resource" "ansible_windows" {
   
   #count                         = "${var.vm_os_offer == "WindowsServer" ? 1 : 0}"
   
-    depends_on = [
+/*     depends_on = [
       null_resource.terraform_sample
-    ]
+    ] */
   provisioner "local-exec" {
     command = "sleep 120"
     }
